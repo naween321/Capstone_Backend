5bec91d5dd0e3ea52ba4af212c0ee9b37dfb2b86
naween321 <naween321@gmail.com>
2026-04-08 19:53:43 -0400

    CICD Test

--- apps/commons/urls.py
+++ apps/commons/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import DeviceTokenView, ScheduleTodoView, DailyQuoteView, AnalyzeMoodView
-
+print("Hello")
 
 urlpatterns = [
     path('token/', DeviceTokenView.as_view(), name='register-token'),
